2dfb4e796b4cecb0376ac434c72fc187b94a3520
Jiamin Zou <jmzou@umich.edu>
2025-11-16 00:47:20 -0500

    Add test: predict.OLS newdata=NULL returns fitted values

--- tests/testthat/test-predict.R
+++ tests/testthat/test-predict.R
@@ -1,8 +1,10 @@
 test_that("predict.OLS generates expected predictions", {
   data(mtcars)
   fit <- lm_fit(mpg ~ wt + hp, mtcars)
-
   preds <- predict(fit, newdata = mtcars)
+
+  preds_nonewdata <- predict(fit)
+  expect_equal(preds_nonewdata, fit$fitted.values, tolerance = 1e-6)
   expect_length(preds, nrow(mtcars))
   expect_true(all(is.finite(preds)))
 })
